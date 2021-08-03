ebe4394cac7388b68eefd9a753c031d9162ba6fb
methylDragon <methylDragon@gmail.com>
2021-08-04 03:00:17 +0800

    Remove k_best_paths

--- neurips2020-flatland-starter-kit/reinforcement_learning/multi_agent_training.py
+++ neurips2020-flatland-starter-kit/reinforcement_learning/multi_agent_training.py
@@ -180,7 +180,7 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
         # root_extra: 15 + k * 9
         # rvnn children: 12 * k_branches
         state_size = (
-            17 + 17
+            # 17 + 17
             + 15 + 5 * 9
             + 12 * 2
             # + 5
@@ -364,7 +364,7 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
                     # hint[hint_agent.act(agent_handle, obs[agent_handle], -1)] = 1
                     state_vector = [
                         # == ROOT ==
-                        *get_k_best_node_states(obs[agent_handle], train_env, num_agents_on_map, obs_params.observation_tree_depth),
+                        # *get_k_best_node_states(obs[agent_handle], train_env, num_agents_on_map, obs_params.observation_tree_depth),
 
                         # == ROOT EXTRA ==
                         train_env.number_of_agents,
@@ -424,7 +424,7 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
                         # hint[hint_agent.act(agent_handle, obs[agent_handle], -1)] = 1
                         state_vector = [
                             # == ROOT ==
-                            *get_k_best_node_states(obs[agent_handle], train_env, num_agents_on_map, obs_params.observation_tree_depth),
+                            # *get_k_best_node_states(obs[agent_handle], train_env, num_agents_on_map, obs_params.observation_tree_depth),
 
                             # == ROOT EXTRA ==
                             train_env.number_of_agents,
@@ -714,7 +714,7 @@ def eval_policy(env, tree_observation, policy, train_params, obs_params):
                             # hint[hint_agent.act(agent, obs[agent], -1)] = 1
                             state_vector = [
                                 # == ROOT ==
-                                *get_k_best_node_states(obs[agent], env, num_agents_on_map, obs_params.observation_tree_depth),
+                                # *get_k_best_node_states(obs[agent], env, num_agents_on_map, obs_params.observation_tree_depth),
 
                                 # == ROOT EXTRA ==
                                 env.number_of_agents,
